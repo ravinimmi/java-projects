9a71d506707dc1801f7a9f1f571b23d31b24f396
Ravi Kumar Nimmi <nimmi.ravikumar@gmail.com>
2018-11-07 16:09:31 +0530

    remove unnecessary code.

--- sudoku-solver/src/main/java/GridExample.java
+++ sudoku-solver/src/main/java/GridExample.java
@@ -1,6 +1,5 @@
 public class GridExample {
     public static void main(String []args) {
-//        generate();
         int[][] cells = {
             {8, 0, 0, 0, 0, 0, 0, 0, 0},
             {0, 0, 3, 6, 0, 0, 0, 0, 0},
@@ -22,20 +21,5 @@ public class GridExample {
         long duration = (endTime - startTime) / 1000000;
         grid.print();
         System.out.println("Time taken to solve = " + duration + " ms");
-    }
-
-    public static void generate() {
-        System.out.println('{');
-        for (int i = 0; i < 9; i++) {
-            System.out.print("\t");
-            System.out.print('{');
-            for (int j = 0; j < 9; j++) {
-                System.out.print("0");
-                if (j != 8)
-                    System.out.print(", ");
-            }
-            System.out.println("},");
-        }
-        System.out.println('}');
     }
 }
